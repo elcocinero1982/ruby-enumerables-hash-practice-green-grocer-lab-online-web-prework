f41c7415405b62ea909e572e9c8afa394594b3be
jose luis <cortezluisjose@gmail.com>
2019-07-06 02:48:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,6 @@
 def consolidate_cart(cart)
   # code here
-  cart = [{"cleaning" => {:wipes => 2.50}}]
+  cart = [{"cleaning" => {:wipes => 2.50, :clearance => true}}, {"cleaning" => towels => 5.00}]
 end
 
 def apply_coupons(cart, coupons)
